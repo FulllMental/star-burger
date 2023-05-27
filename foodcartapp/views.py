efcde2942816a2cfff7d43ad3fa642a17757e0aa
FulllMental <104234625+FulllMental@users.noreply.github.com>
2023-05-27 23:07:48 +0300

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -3,6 +3,7 @@ from django.templatetags.static import static
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from phonenumbers import parse, is_valid_number
 
 from .models import Product, OrderDetails, OrderedProducts
 import json
@@ -60,21 +61,48 @@ def product_list_api(request):
     })
 
 
+def validate_order(new_order):
+    order_keys = ['firstname', 'lastname', 'address']
+    for order_key in order_keys:
+        if not new_order.get(order_key):
+            content = {'error': f'похоже что {order_key} отсутствует или указано значение None/Null/0/False...'}
+            return content, False
+        if not isinstance(new_order[order_key], str):
+            content = {'error': f'получен неподдерживаемый формат данных в параметре "{order_key}"...'}
+            return content, False
+
+    if not new_order.get('phonenumber'):
+        content = {'error': '"phonenumber" отсутствует...'}
+        return content, False
+    client_phonenumber = parse(new_order['phonenumber'], 'RU')
+    if not is_valid_number(client_phonenumber):
+        content = {'error': '"phonenumber" не подходит под формат региона...'}
+        return content, False
+
+    if not new_order.get('products'):
+        content = {'error': 'похоже что список "products" отсутствует или оказался пустым...'}
+        return content, False
+    ordered_products = new_order['products']
+    if not isinstance(ordered_products, list):
+        content = {'error': 'похоже вместо списка "products" получен другой формат данных...'}
+        return content, False
+
+    all_ordered_products = [ordered_product['product'] for ordered_product in ordered_products]
+    all_products = Product.objects.all()
+    last_product_id = list(all_products)[-1].id
+    for product_id in all_ordered_products:
+        if not 0 < int(product_id) <= last_product_id:
+            content = {'error': f'Позиции с индексом {product_id} не существует...'}
+            return content, False
+    return {}, True
+
+
 @api_view(['POST'])
 def register_order(request):
     new_order = request.data
-    # Должен быть +
-    # Не нул +
-    # не пустым
-    # список +
-
-    if not new_order.get('products'):
-        content = {'error': 'похоже что список ["products"] отсутствует или оказался пустым...'}
+    content, validation = validate_order(new_order)
+    if not validation:
         return Response(content, status=status.HTTP_404_NOT_FOUND)
-    if not isinstance(new_order['products'], list):
-        content = {'error': 'похоже вместо списка ["products"] получен другой формат данных...'}
-        return Response(content, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-
     defaults = {
         'lastname': new_order.get('lastname', ''),
     }
